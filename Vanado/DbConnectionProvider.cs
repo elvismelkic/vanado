5881b62e588b697ad2341c56b9b2443a00d15d51
Elvis Melkić <elvis.melkic@gmail.com>
2020-09-14 22:00:02 +0200

    Add database connection provider

---
+++ Vanado/DbConnectionProvider.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+using System.Data;
+using WebApplication3.Interfaces;
+
+namespace WebApplication3
+{
+    public class DbConnectionProvider : IDbConnectionProvider
+    {
+        private readonly string connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=vanado";
+
+        public IDbConnection GetDbConnection()
+        {
+            return new NpgsqlConnection(connectionString);
+        }
+    }
+}
